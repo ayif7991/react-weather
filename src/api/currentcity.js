5354401156d6c45358442f5804b4756e6eca2cb5
alfiya <alfiya060@gmail.com>
2025-05-06 13:23:34 +0200

    Refactor geolocation functionality to use getUserCity and improve error handling; update loading message in App component

--- src/api/currentcity.js
+++ src/api/currentcity.js
@@ -1,30 +1,22 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-
-const getCity = (setCity) => {
-    navigator.geolocation.getCurrentPosition(
+const getUserCity = (setCity) => {
+  navigator.geolocation.getCurrentPosition(
     async (position) => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        const geoUrl = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-        const res = await fetch(geoUrl);
-        const data = await res.json();
-        const city = data.name;
-        setCity(city)
-
-
-        
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      const geoUrl = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+      const res = await fetch(geoUrl);
+      const data = await res.json();
+      const city = data.name;
+      setCity(city);
     },
     (error) => {
-        console.error("Error getting location:", error);
-        setCity('London')
+      console.error("Error getting location:", error);
+      setCity("London");
     }
-    );
-}
+  );
+};
 
-
-
-
-export default getCity;
-    
+export default getUserCity;
--- src/api/weatherdata.js
+++ src/api/weatherdata.js
@@ -4,7 +4,6 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const fetchWeatherData = async (city,setLoading,setForecastData,setError,setWeatherData) => {
     try {
-      console.log("fetching data for:", city);
       setLoading(true);
       setError(null);
       const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
@@ -21,8 +20,7 @@ const fetchWeatherData = async (city,setLoading,setForecastData,setError,setWeat
       );
 
       setForecastData(dailyForecast);
-      console.log("forecastdata:", forecastdata);
-      console.log("dailyForecast:", dailyForecast);
+      
     } catch (error) {
       setError("Failed to fetch weather data. Please try again.");
       console.error("Error fetching weather data:", error);
